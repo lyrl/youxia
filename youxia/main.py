8e428b1525cdd151d9b368d097edb04574ff1965
lyrl <lyrl@me.com>
2016-08-03 01:43:49 +0000

    rename

--- youxia/main.py
+++ youxia/main.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 # Created on 2016-07-26 11:04:34
-import youxia.compoents.youxia_crawler_compoent as yxc
+import sys
+import os
+
+path = '/root/youxia'
+
+if path not in sys.path:
+    sys.path.append(path)
+import compoents.youxia_crawler_compoent as yxc
 
 if __name__ == '__main__':
     yxcrawler = yxc.YouxiaCrawler("youxia.db", create_table=True)
